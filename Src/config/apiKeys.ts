e28211830d7083753d0c3c8e7db06ba5b1273c52
Vignesh Arun Kumar M <vignesharunkumarm@Vigneshs-MacBook-Air.local>
2026-04-05 18:13:32 +0530

    remove keys

--- Src/config/apiKeys.ts
+++ Src/config/apiKeys.ts
@@ -2,8 +2,7 @@ export const MAPBOX_ACCESS_TOKEN = 'key';
 export const AZURE_VISION_ENDPOINT = 'key';
 export const AZURE_VISION_API_KEY = 'key';
 
-export const WEB_CLIENT_ID =
-  '237608787999-aso9qbrd89bhkfpadhc8td0vm8b6ouge.apps.googleusercontent.com';
+export const WEB_CLIENT_ID = 'key';
 
 export function hasMapboxAccessToken() {
   return MAPBOX_ACCESS_TOKEN.trim().length > 0;
